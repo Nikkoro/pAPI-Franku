a53206a8ac82f178a51a93fb4053e72e9c29b531
nikko <nikodemyo@gmail.com>
2022-03-01 11:35:40 +0100

    cors

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const cors = require("cors");
 const corsSettings = {
     origin: "*",
     methods: "GET",
-    optionsSuccessStatus: 204,
+    optionsSuccessStatus: 200,
 };
 const app = express();
 const importData = require("./quotes.json");
+
 let port = process.env.PORT || 3000;
 
 app.get("/", cors(corsSettings), (req, res) => {
     res.send("pAPI Franku");
 });
 
-app.get("/omniverse", cors(corsSettings), (req, res) => {
+app.get("/quotes", cors(corsSettings), (req, res) => {
     res.send(importData);
+});
+
+app.get("/quote", cors(corsSettings), (req, res) => {
+    const randomQuote = importData[Math.round(Math.random() * 50)];
+    res.send(randomQuote);
 });
 
 app.listen(port, () => {
